8d3d8e0d7d689bf558e34add28a43a66da3d1177
Gopal <gsudhanshu12@gmail.com>
2017-12-30 12:01:15 -0500

    ngStyle directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,7 +8,13 @@ export class ServerComponent{
 serverid: number=10;
 serverStatus: string='offline';
 
+constructor(){
+    this.serverStatus=Math.random() >.5 ? 'online':'offline';
+}
 getServerStatus(){
     return this.serverStatus;
+}
+getColor(){
+    return this.serverStatus==='online' ? 'green':'red';
 }
 }
