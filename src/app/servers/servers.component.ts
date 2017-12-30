e6581ed64a02f376a0d7e5fba272d2367ddff33f
Gopal <gsudhanshu12@gmail.com>
2017-12-30 12:33:48 -0500

    ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,13 +4,19 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
  // template: `<app-server></app-server>`,
  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles:[`
+  .online{
+    color:white;
+  }
+  `]
 })
 export class ServersComponent implements OnInit {
 allowNewServer=false;
 serverCreationStatus='No server was created'
 serverName='Test';
 serverCreated=false;
+servers=['testServer','productionServer']
 
   constructor() { 
     setTimeout(()=>{
@@ -23,6 +29,7 @@ serverCreated=false;
 
   onCreateServer(){
     this.serverCreated=true
+    this.servers.push(this.serverName)
     this.serverCreationStatus='Server was created';
   }
 
